02ea4ab9c0d04813f8f82e310ea8a080678dd66b
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-05-07 22:08:15 +0530

    Update login.py

--- login.py
+++ login.py
@@ -4,13 +4,13 @@ driver = webdriver.Chrome()
 driver.get("https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fmail.google.com&service=mail&ec=GAlAFw&flowName=GlifWebSignIn&flowEntry=AddSession")
 
 input=driver.find_element_by_xpath('//*[@id="identifierId"]')
-input.send_keys('vkdk123@gmail.com')
+input.send_keys('vkdk123@gmail.com\n')
 
 input=driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span')
 input.click()
 
 input=driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
-input.send_keys('98765432')
+input.send_keys('98765432\n')
 
 input=driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[3]')
 input.click()
